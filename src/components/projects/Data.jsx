8312fece2cab203a5856bc9ff7e9e5a52dd59192
Ossama-Dev07 <ossamadev22@gmail.com>
2024-09-05 23:14:38 +0100

    v1.1

--- src/components/projects/Data.jsx
+++ src/components/projects/Data.jsx
@@ -1,43 +1,58 @@
 import Project1 from "../../assets/project1.webp";
 import Project2 from "../../assets/project2.webp";
 import Project3 from "../../assets/project3.webp";
-
+import Project4 from "../../assets/project4.webp";
+import Project5 from "../../assets/project5.webp";
 
 export const projectsData = [
   {
     id: 1,
     image: Project1,
-    title: "Project 1",
-    url: "#1",
+    title: "SYBALINK-social media",
+    url: "https://github.com/Ossama-Dev07/sybalink-social-app",
     category: "A",
   },
   {
     id: 2,
     image: Project2,
-    title: "Project 2",
-    url: "#2",
+    title: "E-commerce web site",
+    url: "https://github.com/Ossama-Dev07/E-Commerce_React",
+    category: "B",
+  },
+  {
+    id: 5,
+    image: Project5,
+    title: "Recipe API project.",
+    url: "https://github.com/Ossama-Dev07/Recipe-API-project",
     category: "B",
   },
   {
     id: 3,
     image: Project3,
-    title: "Project 3",
-    url: "#3",
+    title: "Product Management",
+    url: "https://github.com/Ossama-Dev07/product-management",
+    category: "C",
+  },
+  {
+    id: 4,
+    image: Project4,
+    title: "Intern Management",
+    url: "https://github.com/Ossama-Dev07/Intern-Management",
     category: "C",
   },
 ];
 
 export const projectsNav = [
-    {
-        name: "All",
-    },
-    {
-        name: "A",
-    },
-    {
-        name: "B",
-    },
-    {
-        name: "C",
-    },
+  {
+    name: "All",
+  },
+  {
+    name: "A",
+  },
+  {
+    name: "B",
+  },
+  {
+    name: "C",
+  },
 ];
--- src/components/projects/ProjectItems.jsx
+++ src/components/projects/ProjectItems.jsx
@@ -6,7 +6,7 @@ const ProjectItems = ({item}) => {
         <div className="project__card" key={item.id}>
             <img className="project__img" src={item.image} alt="" />
             <h3 className="project__title">{item.title}</h3>
-            <a href={item.url} className="project__button">
+            <a href={item.url} target='blank' className="project__button">
                 Demo <HiOutlineArrowSmRight className="project__button-icon" />
             </a>
         </div>
